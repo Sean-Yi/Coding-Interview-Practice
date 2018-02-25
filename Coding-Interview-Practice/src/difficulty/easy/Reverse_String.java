1d027ec5581e00ab83184bc3ac28bb339cff9785
Seon-Yi <seonbok2@gmail.com>
2018-02-25 00:25:06 -0500

    Reverse_String

---
+++ Coding-Interview-Practice/src/difficulty/easy/Reverse_String.java
@@ -0,0 +1,15 @@
+package difficulty.easy;
+
+public class Reverse_String {
+
+	
+    public String reverseString(String s) {
+        StringBuilder res = new StringBuilder();
+        for(int i = s.length()-1; i >= 0; i--) {
+        	res.append(s.charAt(i));
+        }
+       
+        return res.toString();
+    } 
+
+}
