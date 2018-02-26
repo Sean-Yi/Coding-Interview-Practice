02d2457b2d0faacf8278c0a164feff9d25a5c2b0
Seon-Yi <seonbok2@gmail.com>
2018-02-26 02:10:56 -0500

    Validate_Binary_Search_Tree

---
+++ Coding-Interview-Practice/src/difficulty/medium/Validate_Binary_Search_Tree.java
@@ -0,0 +1,26 @@
+package difficulty.medium;
+
+public class Validate_Binary_Search_Tree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public boolean isValidBST(TreeNode root) {
+        
+        return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    public boolean isValidBST(TreeNode root, int leftVal, int rightVal) {
+        if(root == null) return true;
+        
+        if(root.val < rightVal && root.val > leftVal) {
+            return  isValidBST(root.left, leftVal, root.val) && isValidBST(root.right, root.val, rightVal);   
+        } else {
+            return false;
+        }
+
+    }
+
+}
