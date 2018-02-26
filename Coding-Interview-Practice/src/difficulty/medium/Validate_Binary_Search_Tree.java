b6543c8f57a71194000cb787e397f122ca6bd5e4
Seon-Yi <seonbok2@gmail.com>
2018-02-26 02:34:41 -0500

    Validate_Binary_Search_Tree version 2

--- Coding-Interview-Practice/src/difficulty/medium/Validate_Binary_Search_Tree.java
+++ Coding-Interview-Practice/src/difficulty/medium/Validate_Binary_Search_Tree.java
@@ -5,19 +5,22 @@ import entities.TreeNode;
 public class Validate_Binary_Search_Tree {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Validate_Binary_Search_Tree s = new Validate_Binary_Search_Tree();
+		TreeNode root =  new TreeNode(1);
+		root.left = new TreeNode(2);
+		s.isValidBST(root);
 
 	}
 	
     public boolean isValidBST(TreeNode root) {
         
-        return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        return isValidBST(root, null, null);
     }
     
-    public boolean isValidBST(TreeNode root, int leftVal, int rightVal) {
+    public boolean isValidBST(TreeNode root, Integer leftVal, Integer rightVal) {
         if(root == null) return true;
         
-        if(root.val < rightVal && root.val > leftVal) {
+        if((rightVal == null || root.val < rightVal) && (leftVal == null || root.val > leftVal)) {
             return  isValidBST(root.left, leftVal, root.val) && isValidBST(root.right, root.val, rightVal);   
         } else {
             return false;
